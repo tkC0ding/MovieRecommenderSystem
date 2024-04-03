2c57aa01fe37702e0948fbc651132c62062d0466
tkC0ding <tkjagani@gmail.com>
2024-04-03 16:51:15 +0530

    Updated Code File
    Saved similarity matrix and imported it into app file

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 import pandas as pd
 import streamlit as st
+import pickle
+
+similarity = pickle.load(open('similarity.pkl', 'rb'))
 
 st.title("Movie Recommender System")
 
 df = pd.read_csv("PreprocessedData/train_data.csv")
 movie_list = tuple(df['title'].values)
+
+def recommend(movie):
+    movie_index = df[df['title'] == movie].index[0]
+    distance = similarity[movie_index]
+    movie_list = sorted(list(enumerate(distance)), reverse=True, key=lambda x:x[1])[1:6]
+
+    r = []
+    for i in movie_list:
+        index = i[0]
+        r.append(df.iloc[index, 1])
+    return(r)
 
 option = st.selectbox(
     'What Movies would you like to watch?',
