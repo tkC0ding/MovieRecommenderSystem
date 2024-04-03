37a3e294f84d4df9ec5e02d621e21740befb2e3e
tkC0ding <tkjagani@gmail.com>
2024-04-03 21:55:15 +0530

    Updated Code File
    Made the final project ready for deployment

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import pandas as pd
 import streamlit as st
 import pickle
 import requests
+import ast
+import json
 
 def fetch_poster(id):
     url = f"https://api.themoviedb.org/3/movie/{id}?language=en-US"
@@ -12,9 +14,8 @@ def fetch_poster(id):
     }
     response = requests.get(url, headers=headers)
     data = response.text
-
+    data = json.loads(data)
     image_path = "https://image.tmdb.org/t/p/w500" + data['poster_path']
-
     return(image_path)
 
 
@@ -28,7 +29,7 @@ movie_list = tuple(df['title'].values)
 def recommend(movie):
     movie_index = df[df['title'] == movie].index[0]
     distance = similarity[movie_index]
-    movie_list = sorted(list(enumerate(distance)), reverse=True, key=lambda x:x[1])[1:6]
+    movie_list = sorted(list(enumerate(distance)), reverse=True, key=lambda x:x[1])[0:6]
 
     r = []
     for i in movie_list:
@@ -42,21 +43,30 @@ option = st.selectbox(
 
 if st.button("Recommend"):
     recommended = recommend(option)
+    poster_list = []
     id_list = []
     for i in recommended:
         id_temp = list(df.loc[df['title'] == i, 'movie_id'])
         id = id_temp[0]
-        id_list.append(id)
+        poster_list.append(fetch_poster(id=id))
     
-    col1, col2, col3, col4, col5 = st.columns(5)
+    col1, col2, col3, col4, col5, col6 = st.columns(6)
 
     with col1:
-        st.write(id_list[0], recommended[0])
+        st.image(poster_list[0])
+        st.write(recommended[0])
     with col2:
-        st.write(id_list[1], recommended[1])
+        st.image(poster_list[1])
+        st.write(recommended[1])
     with col3:
-        st.write(id_list[2], recommended[2])
+        st.image(poster_list[2])
+        st.write(recommended[2])
     with col4:
-        st.write(id_list[3], recommended[3])
+        st.image(poster_list[3])
+        st.write(recommended[3])
     with col5:
-        st.write(id_list[4], recommended[4])
+        st.image(poster_list[4])
+        st.write(recommended[4])
+    with col6:
+        st.image(poster_list[5])
+        st.write(recommended[5])
